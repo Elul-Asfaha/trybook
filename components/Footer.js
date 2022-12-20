7f68d1e5e2fdbbbb039f4c1065655b0d407a209b
Elul-Asfaha <69459338+Elul-Asfaha@users.noreply.github.com>
2022-12-21 01:37:22 +0300

    Delete components directory

--- components/Footer.js
+++
@@ -1,8 +0,0 @@
-const Footer=()=>{
-    return(
-        <footer className="p-4 text-white bg-slate-900 w-full text-center text-gray-500">
-            copyright by scorp, powered by openlibrary Api
-        </footer>
-    )
-}
-export default Footer
